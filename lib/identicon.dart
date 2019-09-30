5dd0269d19cf78c4c9ecee6b528c9fa3304be319
phinexdaz <27595954@qq.com>
2019-09-30 19:49:30 +0800

    update

--- lib/identicon.dart
+++ lib/identicon.dart
@@ -7,49 +7,50 @@ import 'dart:typed_data';
 import 'package:crypto/crypto.dart';
 import 'package:image/image.dart';
 
+var colourCache = Map<String, List<List<int>>>();
+
 class Identicon {
   int _rows;
   int _cols;
 
   Function(List<int>) _digest;
 
-  List<int> _fg_colour;
-  List<int> _bg_colour;
+  List<int> _fgColour;
+  List<int> _bgColour;
 
   Identicon({int rows = 6, int cols = 6}) {
     this._rows = rows;
     this._cols = cols;
 
-    this._generate_colours();
     this._digest = md5.convert;
   }
 
-  _generate_colours() {
-    var colours_ok = false;
+  _generateColours(String cacheKey) {
+    var coloursOk = false;
 
-    while (!colours_ok) {
-      this._fg_colour = this._get_pastel_colour();
-      if (this._bg_colour == null) {
-        this._bg_colour = this._get_pastel_colour(lighten: 80);
+    if (colourCache.containsKey(cacheKey)) {
+      this._fgColour = colourCache[cacheKey][0];
+      this._bgColour = colourCache[cacheKey][1];
+    } else {
+      while (!coloursOk) {
+        this._fgColour = this._getPastelColour();
+        if (this._bgColour == null) {
+          this._bgColour = this._getPastelColour(lighten: 80);
 
-        var fg_lum = this._luminance(this._fg_colour) + 0.05;
-        var bg_lum = this._luminance(this._bg_colour) + 0.05;
-        if (fg_lum / bg_lum > 1.20) {
-          colours_ok = true;
+          var fgLum = this._luminance(this._fgColour) + 0.05;
+          var bgLum = this._luminance(this._bgColour) + 0.05;
+          if (fgLum / bgLum > 1.20) {
+            coloursOk = true;
+          }
+        } else {
+          coloursOk = true;
         }
-      } else {
-        colours_ok = true;
       }
+      colourCache[cacheKey] = [this._fgColour, this._bgColour];
     }
   }
 
-  _get_image(String text, int width, int height, {int pad = 0}) {
-    var hex_digest_byte_list = this._string_to_byte_list(text);
-    var matrix = this._create_matrix(hex_digest_byte_list);
-    return this._create_image(matrix, width, height, pad);
-  }
-
-  _get_pastel_colour({int lighten = 127}) {
+  _getPastelColour({int lighten = 127}) {
     var r = () => Random().nextInt(128) + lighten;
     return [r(), r(), r()];
   }
@@ -61,47 +62,37 @@ class Identicon {
       var result = (v < 0.03928) ? v / 12.92 : pow(((v + 0.055) / 1.055), 2.4);
       a.add(result);
     }
-    
+
     return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
   }
 
-  _string_to_byte_list(String data) {
-    var bytes_length = 16;
-    var hex_digest = this._digest(utf8.encode(data)).toString();
-
-    return List<int>.generate(bytes_length, (int i) {
-      return int.parse(hex_digest.substring(i * 2, i * 2 + 2),
-          radix: bytes_length);
-    });
-  }
-
-  _bit_is_one(int n, List<int> hash_bytes) {
+  _bitIsOne(int n, List<int> hashBytes) {
     var scale = 16;
-    return hash_bytes[n ~/ (scale / 2)] >>
-                ((scale / 2) - ((n % (scale / 2)) + 1)).toInt() &
-            1 ==
+    return hashBytes[n ~/ (scale / 2)] >>
+        ((scale / 2) - ((n % (scale / 2)) + 1)).toInt() &
+    1 ==
         1;
   }
 
-  _create_image(List<List<bool>> matrix, int width, int height, int pad) {
+  _createImage(List<List<bool>> matrix, int width, int height, int pad) {
     var image = Image.rgb(width + (pad * 2), height + (pad * 2));
-    image.fill(
-        Color.fromRgb(this._bg_colour[0], this._bg_colour[1], this._bg_colour[2]));
+    image.fill(Color.fromRgb(
+        this._bgColour[0], this._bgColour[1], this._bgColour[2]));
 
-    var block_width = width ~/ this._cols;
-    var block_height = height ~/ this._rows;
+    var blockWidth = width ~/ this._cols;
+    var blockHeight = height ~/ this._rows;
 
     for (int row = 0; row < matrix.length; row++) {
       for (int col = 0; col < matrix[row].length; col++) {
         if (matrix[row][col]) {
           fillRect(
             image,
-            pad + col * block_width,
-            pad + row * block_height,
-            pad + (col + 1) * block_width - 1,
-            pad + (row + 1) * block_height - 1,
+            pad + col * blockWidth,
+            pad + row * blockHeight,
+            pad + (col + 1) * blockWidth - 1,
+            pad + (row + 1) * blockHeight - 1,
             Color.fromRgb(
-                this._fg_colour[0], this._fg_colour[1], this._fg_colour[2]),
+                this._fgColour[0], this._fgColour[1], this._fgColour[2]),
           );
         }
       }
@@ -109,24 +100,35 @@ class Identicon {
     return writePng(image);
   }
 
-  _create_matrix(List<int> byte_list) {
+  _createMatrix(List<int> byteList) {
     var cells = (this._rows * this._cols / 2 + this._cols % 2).toInt();
-    var matrix =
-        List.generate(this._rows, (_) => List.generate(this._cols, (_) => false));
+    var matrix = List.generate(
+        this._rows, (_) => List.generate(this._cols, (_) => false));
 
     for (int n = 0; n < cells; n++) {
       if (this
-          ._bit_is_one(n, byte_list.getRange(1, byte_list.length).toList())) {
-        var x_row = n % this._rows;
-        var y_col = n ~/ this._cols;
-        matrix[x_row][this._cols - y_col - 1] = true;
-        matrix[x_row][y_col] = true;
+          ._bitIsOne(n, byteList.getRange(1, byteList.length).toList())) {
+        var row = n % this._rows;
+        var col = n ~/ this._cols;
+        matrix[row][this._cols - col - 1] = true;
+        matrix[row][col] = true;
       }
     }
     return matrix;
   }
 
   Uint8List generate(String text, {int size = 36}) {
-    return this._get_image(text, size, size, pad: (size * 0.1).toInt());
+    var bytesLength = 16;
+    var hexDigest = this._digest(utf8.encode(text)).toString();
+
+    var hexDigestByteList = List<int>.generate(bytesLength, (int i) {
+      return int.parse(hexDigest.substring(i * 2, i * 2 + 2),
+          radix: bytesLength);
+    });
+
+    this._generateColours(hexDigest);
+
+    var matrix = this._createMatrix(hexDigestByteList);
+    return this._createImage(matrix, size, size, (size * 0.1).toInt());
   }
 }
